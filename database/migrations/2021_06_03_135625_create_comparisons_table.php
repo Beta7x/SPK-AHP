62691c43ae9d420ae97c90e08cfb3ccd8a8bd47e
Beta7x <adepriyantowidies@gmail.com>
2023-04-16 22:31:05 +0700

    First commit

---
+++ database/migrations/2021_06_03_135625_create_comparisons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComparisonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comparisons', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipe')->unique();
+            $table->string('harga');
+            $table->string('kandugan');
+            $table->string('bentuk');
+            $table->string('rasa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comparisons');
+    }
+}
